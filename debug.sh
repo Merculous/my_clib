80f6c39da0584173afdb79b919aaad32268da07a
Merculous <24739823+Merculous@users.noreply.github.com>
2023-03-07 12:56:20 +0000

    Update debug.sh (add another valgrind parameter for extra debugging).

--- debug.sh
+++ debug.sh
@@ -1,7 +1,3 @@
 #!/bin/sh
 
-if [ "$#" -ne 1 ]; then
-    echo "Usage: <executable>"
-else
-    valgrind --leak-check=full --show-leak-kinds=all "$1"
-fi
+valgrind --leak-check=full --show-leak-kinds=all -s ./tool kernelcache.encrypted
