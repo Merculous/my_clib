4a19bcb9988630085279c4e4d83bb674e58218ee
Merculous <24739823+Merculous@users.noreply.github.com>
2023-03-08 09:18:05 +0000

    Patch according to address sanitizer a static buffer overflow. Tl;DR I pasted the buffer size allocation number to the index of buffer (i = 5) which is BAD! Later I used that number to WRITE a '\0' to the index 5, mistakenly, which should've been 4 because sizeof(uint32_t) is 4. This is important because we iterate from 0->n, using index 5 is really trying to access a 6th char, which is not in our array. This is just a simple fix: remove the NULL from the operation. See I'm an ultra C NOOB :P

--- src/string_utils.c
+++ src/string_utils.c
@@ -3,9 +3,9 @@
 
 const unsigned char *convertUInt32ToASCII(const uint32_t value)
 {
-    static unsigned char buffer[sizeof(value) + sizeof(char)];
+    static unsigned char buffer[sizeof(char) * sizeof(value) + sizeof(char)];
     memcpy(buffer, &value, sizeof(value));
-    buffer[sizeof(value) + sizeof(char)] = '\0';
+    buffer[sizeof(char) * sizeof(value)] = '\0';
     static const unsigned char *bufferPointer = (const unsigned char *)buffer;
     return bufferPointer;
 }
